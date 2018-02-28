434a6ff20365e264f86ea02bef7201d0f064e7c5
Johny Nguyen <manh.1403@gmail.com>
2018-03-01 00:48:58 +0700

    Update...

--- src/main/java/com/elastic/search/search/ServerConst.java
+++ src/main/java/com/elastic/search/search/ServerConst.java
@@ -1,6 +1,10 @@
 package com.elastic.search.search;
 
 public final class ServerConst {
-	public static final String HOST = "192.168.1.40";
+	/*
+	 * ElasticSearch with default URL: publish_address {127.0.0.1:9300},
+	 * bound_addresses {127.0.0.1:9300}, {[::1]:9300}
+	 */
+	public static final String HOST = "127.0.0.1";
 	public static final int PORT = 9300;
 }
